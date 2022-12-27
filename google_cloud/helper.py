18e79f01381ade8c622f7e3d9923c55afa4d5e7a
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2022-12-27 14:32:30 +0700

    update #1

---
+++ google_cloud/helper.py
@@ -0,0 +1,32 @@
+import os
+import common.constants as Constant
+
+
+def delete_temp():
+    for root, _, files in os.walk(Constant.TEMP_FOLDER):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                os.remove(file_path)
+            except (Exception, ValueError):
+                pass
+
+
+def change_name(name):
+    return name.strip('.')
+
+
+def write_key(key, email):
+    with open('google_key.txt', 'a') as file:
+        file.write(email + ":" + key + '\n')
+
+
+def write_error(error, email, name_project):
+    with open('error_email.txt', 'a') as file:
+        file.write(email + ":" + error + ":" + name_project + '\n')
+
+
+def check_number(email):
+    if email[0].isdigit():
+        return True
+    return False
--- google_cloud/key.py
+++ google_cloud/key.py
@@ -4,12 +4,13 @@ import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.common.by import By
 import common.constants as Constant
 from time import sleep
+import google_cloud.helper as Helper
 
 
 class GoogleApiKey:
-    def __init__(self, output_file):
-        self.file = output_file + '.txt'
+    def __init__(self):
         self.driver = None
+        self.error = None
 
     def create_project(self, project_name):
         driver = self.driver
@@ -19,9 +20,12 @@ class GoogleApiKey:
         driver.execute_script("window.scrollBy(0,1530)", "")
         sleep(Constant.SHORT_WAIT)
         window_before = driver.window_handles[0]
-        driver.switch_to.frame(
-            driver.find_element(By.CSS_SELECTOR, ".apis-explorer > iframe"))
-        driver.find_element(By.ID, "execute").click()
+        try:
+            driver.switch_to.frame(
+                driver.find_element(By.CSS_SELECTOR, ".apis-explorer > iframe"))
+            driver.find_element(By.ID, "execute").click()
+        except:
+            self.error = "Cantswitchtoframe"
         try:
             window_after = driver.window_handles[1]
             driver.switch_to.window(window_after)
@@ -33,33 +37,78 @@ class GoogleApiKey:
             driver.find_element(By.CSS_SELECTOR, enable).click()
             driver.switch_to.window(window_before)
         except:
-            pass
-        sleep(10)
+            self.error = "Cantswitchtochooseaccount"
+        sleep(Constant.STAND_BY_TIMEOUT)
 
     def create_key(self, project_name):
         driver = self.driver
-        driver.get(
-            "https://console.cloud.google.com/welcome?project=" + project_name)
-        sleep(100)
-        xpath = '//*[@id="cfctest-section-nav-item-metropolis_api_credentials"]'
-        create = '_0rif_action-bar-create-button'
-        WebDriverWait(driver, Constant.LOADING_TIMEOUT).until(EC.visibility_of_element_located(
-            (By.ID, create)))
-        driver.find_element(By.ID, create).click()
-        sleep(10)
+        try:
+            driver.get(
+                "https://console.cloud.google.com/apis/credentials?project=" + project_name)
+            create = '_0rif_action-bar-create-button'
+            WebDriverWait(driver, Constant.LOADING_TIMEOUT).until(EC.visibility_of_element_located(
+                (By.ID, create)))
+            driver.find_element(By.ID, create).click()
+            driver.execute_script("document.getElementsByClassName('cfc-menu-item-has-note')[0].click()")
+            sleep(Constant.STAND_BY_TIMEOUT)
+            api_key = driver.execute_script("return document.getElementById('_0rif_mat-input-0').value")
+            return str(api_key)
+        except:
+            self.error = "project_invalid"
 
     def create_multi_projects(self):
         f = open("accounts.txt", "r")
         for email in f:
             email = email.split(':')
+            if Helper.check_number(email[0].split('@')[0]):
+                continue
             try:
                 profile = ChromeProfile(email[0], email[1], email[2])
                 self.driver = profile.retrieve_driver()
                 profile.start()
             except:
+                self.error = "CantsignintoGoogleAccount"
                 continue
             for num in range(1, 11):
                 name = email[0].split('@')[0]
-                # self.create_project(project_name=name + 'test1' + str(num))
-                self.create_key(project_name=name + 'test1' + str(num))
+                name = Helper.change_name(name)
+                self.create_project(project_name=name + str(num))
+                if self.error is not None:
+                    Helper.write_error(error=self.error, email=email, name_project=(name + str(num)))
+                    self.error = None
+            Helper.delete_temp()
 
+    def get_key(self):
+        f = open("accounts.txt", "r")
+        for email in f:
+            email = email.split(':')
+            if Helper.check_number(email[0].split('@')[0]):
+                continue
+            try:
+                profile = ChromeProfile(email[0], email[1], email[2])
+                self.driver = profile.retrieve_driver()
+                profile.start()
+            except:
+                self.error = "CantsignintoGoogleAccount"
+                continue
+            try:
+                self.google_terms()
+            except:
+                pass
+            for num in range(1, 11):
+                name = email[0].split('@')[0]
+                name = Helper.change_name(name)
+                key = self.create_key(name + str(num))
+                if self.error is None:
+                    Helper.write_key(key=key, email=email[0])
+                else:
+                    Helper.write_error(error=self.error, email=email[0], name_project=(name + str(num)))
+                    self.error = None
+            Helper.delete_temp()
+
+    def google_terms(self):
+        driver = self.driver
+        driver.get('https://console.cloud.google.com')
+        sleep(Constant.STAND_BY_TIMEOUT)
+        driver.execute_script("document.getElementById('mat-mdc-checkbox-2-input').click()")
+        driver.execute_script("document.getElementsByClassName('mat-primary')[6].click()")
